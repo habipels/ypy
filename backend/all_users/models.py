70a3d34a80987fbe0eed4f0f832374abaa233c8f
Habip ELİŞ <habibelis65@gmail.com>
2022-05-27 14:26:41 +0300

    #11 backend django user app

---
+++ backend/all_users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class akademisyen(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    isim_soyisim = models.CharField(max_length=100)
+    def __str__(self):
+        return self.isim_soyisim
+
+class ogrenci(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    isim_soyisim = models.CharField(max_length=100)
+    def __str__(self):
+        return self.isim_soyisim
