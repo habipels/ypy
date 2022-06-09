8ea4ec071fc3eb0e946838d59fa111b615f0fde1
Habip ELİŞ <habibelis65@gmail.com>
2022-06-09 11:14:24 +0300

    #views update

--- backend/user_op/views.py
+++ backend/user_op/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse,redirect
+from django.utils.datastructures import MultiValueDictKeyError
+from .forms import RegisterForm,LoginForm
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import login,authenticate,logout
 # Create your views here.
