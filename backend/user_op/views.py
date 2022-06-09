9dd67214dcfb27f82855b1366a8fde8c22792cfd
Habip ELİŞ <habibelis65@gmail.com>
2022-06-09 11:15:42 +0300

    Update views.py

--- backend/user_op/views.py
+++ backend/user_op/views.py
@@ -6,3 +6,17 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import login,authenticate,logout
 # Create your views here.
+def giris(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form":form
+    }
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(username = username,password = password)
+        if user is None:
+            return #login front
+        login(request,user)
+        return redirect("index")
+    return  #login front
