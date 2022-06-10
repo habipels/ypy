fbbe62d8ddfa947de2423368e23c617eccc7920f
AhmetEY <ahmetenesyegin@gmail.com>
2022-06-10 17:11:57 +0300

    #views modul import

--- backend/makale/views.py
+++ backend/makale/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse,redirect,get_object_or_404,reverse
+from .forms import ArticleForm
+from .models import Article,Comment
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 # Create your views here.
