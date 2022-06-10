081c7b59e81409260b90f0026658d2ed080f425c
Habip ELİŞ <habibelis65@gmail.com>
2022-06-10 16:46:55 +0300

    #update views

--- backend/user_op/views.py
+++ backend/user_op/views.py
@@ -20,3 +20,23 @@ def giris(request):
         login(request,user)
         return redirect("index")
     return  #login front
+
+def kayit(request):
+    form = RegisterForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        email = form.cleaned_data.get("email")
+        newUser = User(username =username,email=email)
+        newUser.set_password(password)
+        newUser.save()
+        login(request,newUser)
+        return redirect("index")
+    context = {
+            "form" : form
+        }
+    return render(request,"user_temp/register.html",context)
+
+def cikis(request):
+    logout(request)
+    return redirect("index")
